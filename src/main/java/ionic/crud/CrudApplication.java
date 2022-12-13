cfa1d0009823708fc164407da1a57a70da432716
sauravionic <srivastava2000saurav@gmail.com>
2022-12-13 13:41:12 +0530

    Added required credential for mondodb. Closes #3

--- src/main/java/ionic/crud/CrudApplication.java
+++ src/main/java/ionic/crud/CrudApplication.java
@@ -1,13 +1,23 @@
 package ionic.crud;
 
+import ionic.crud.Models.Student;
+import ionic.crud.Services.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CrudApplication {
+public class CrudApplication implements CommandLineRunner {
 
+	@Autowired
+	private StudentRepository studentRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(CrudApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+//		studentRepository.save(new Student(1L, "Saurav", "Srivastava", "Sarnath", ""))
+	}
 }
--- src/main/java/ionic/crud/Models/Student.java
+++ src/main/java/ionic/crud/Models/Student.java
@@ -1,11 +1,23 @@
 package ionic.crud.Models;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("student")
 public class Student {
 
+    @Id
     private Long id;
     private String firstName;
     private String lastName;
     private String address;
     private String phoneNo;
 
+    public Student(Long id, String firstName, String lastName, String address, String phoneNo) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.phoneNo = phoneNo;
+    }
 }
