b3e4c2237433cfb607dfec62e1057a61c4285de0
sauravionic <srivastava2000saurav@gmail.com>
2022-12-13 13:01:35 +0530

    Initialized Project. Added Student Model. Closes #1

---
+++ src/main/java/ionic/crud/Models/Student.java
@@ -0,0 +1,11 @@
+package ionic.crud.Models;
+
+public class Student {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String phoneNo;
+
+}
